d8ce79b88a09cf2572e3a5edf3e0c4d3699a6cb9
elcarp <lise.carpenter@gmail.com>
2025-02-14 10:57:54 +0700

    WIP resolve build errors

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,53 +1,66 @@
 'use client'
-import React from 'react'
+import React, { Suspense } from 'react'
 import useGitHubRepos from '~hooks/useGitHubRepos'
 import { useRouter, useSearchParams } from 'next/navigation'
 import styles from './page.module.css'
 import Image from 'next/image'
-import { Suspense } from 'react'
 
 export default function User() {
-  const searchParams = useSearchParams()
+  // const searchParams = useSearchParams()
 
-  const username = searchParams.get('username') || ''
+  // const username = searchParams.get('username') || ''
 
-  const { loading, error, user, repos } = useGitHubRepos(username)
   const router = useRouter()
-  console.log(repos)
+
+  const UserComponent = () => {
+    return (
+      <Suspense fallback={<p>Loading user details...</p>}>
+        <UserDetails />
+      </Suspense>
+    )
+  }
+
+  const UserDetails = () => {
+    const searchParams = useSearchParams()
+    const username = searchParams.get('username')
+    const { loading, error, user, repos } = useGitHubRepos(username || '')
+
+    return <p>Username: {user && user.login}</p>
+  }
   return (
     <>
-      <Suspense fallback={<p>Loading...</p>}>
-        <span style={{ cursor: 'pointer' }} onClick={() => router.push('/')}>
-          &larr; Go back
-        </span>
-        <div className={styles.page}>
+      <span style={{ cursor: 'pointer' }} onClick={() => router.push('/')}>
+        &larr; Go back
+      </span>
+      <div className={styles.page}>
+        <div>
           <div>
-            <div>
-              {loading && <p>loading...</p>}
-              {error && <p>{error}</p>}
-              {user && (
-                <Image
-                  src={user.avatar_url}
-                  alt={user.login}
-                  width={100}
-                  height={100}
-                  style={{
-                    borderRadius: '4rem',
-                    border: 'solid 2px black',
-                    margin: 'auto',
-                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-                  }}
-                />
-              )}
-              Repositories: {repos && repos.length}
-              {/* {repos &&
+            {/* {loading && <p>loading...</p>}
+            {error && <p>{error}</p>} */}
+            {/* {user && (
+              <Image
+                src={user.avatar_url}
+                alt={user.login}
+                width={100}
+                height={100}
+                style={{
+                  borderRadius: '4rem',
+                  border: 'solid 2px black',
+                  margin: 'auto',
+                  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+                }}
+              />
+            )} */}
+            <UserComponent />
+            Repositories: 
+            {/* {repos && repos.length} */}
+            {/* {repos &&
               repos.map((repo) => {
                 return <>{repo}</>
               })} */}
-            </div>
           </div>
         </div>
-      </Suspense>
+      </div>
     </>
   )
 }
