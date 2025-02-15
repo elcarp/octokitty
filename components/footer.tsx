a6e2b1e5fa3f3cca8123c74d42e9b61190b4c9d3
elcarp <lise.carpenter@gmail.com>
2025-02-15 16:36:27 +0700

    refactor

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,22 +1,16 @@
 import { Mansalva } from 'next/font/google'
 import React from 'react'
+import styles from './footer.module.css' 
 
-const mansalva = Mansalva({
-  subsets: ['latin'],
-  weight: '400',
-})
+const mansalva = Mansalva({ subsets: ['latin'], weight: '400' })
 
 export default function Footer() {
   return (
-    <>
-      <footer className={mansalva.className}>
-        <div
-          className='footer'
-          style={{ textAlign: 'center', display: 'flex', alignItems: 'end' }}>
-          Designed and developed with love by Lise, a heartfelt tribute to
-          Annapurna 🐈‍⬛
-        </div>
-      </footer>
-    </>
+    <footer className={`${mansalva.className} ${styles.footer}`}>
+      <p>
+        Designed and developed with ❤️ by Lise, a heartfelt tribute to Annapurna
+        🐈‍⬛
+      </p>
+    </footer>
   )
 }
