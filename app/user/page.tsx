fc9779a59addaa1938a4e29a291ae9430dbf6304
elcarp <lise.carpenter@gmail.com>
2025-02-15 15:05:58 +0700

    optimize

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -6,45 +6,37 @@ import { useLanguage } from '~context/LanguageContext'
 import UserDetails from './userdetails'
 import { Mansalva } from 'next/font/google'
 
-const mansalva = Mansalva({
-  subsets: ['latin'],
-  weight: '400',
-})
+const mansalva = Mansalva({ subsets: ['latin'], weight: '400' })
 
 export default function User() {
   const router = useRouter()
-  const language = useLanguage()
+  const { language } = useLanguage()
 
-  const UserComponent = () => {
-    return (
-      <Suspense fallback={<p>Loading user details...</p>}>
-        <UserDetails />
-      </Suspense>
-    )
+  const getText = () => (language === 'en' ? 'Go back' : 'Go back, meow')
+
+  const containerStyle: React.CSSProperties = {
+    margin: 'auto',
+    marginTop: '2rem',
+    display: 'flex',
+    alignItems: 'top',
+    minHeight: '80vh',
   }
 
   return (
-    <>
-      <div className={`${styles.page}`}>
-        <div
-          style={{
-            margin: 'auto',
-            marginTop: '2rem',
-            display: 'flex',
-            alignItems: 'top',
-            minHeight: '80vh',
-          }}>
-          <div>
-            <span
-              style={{ cursor: 'pointer', fontSize: '3rem' }}
-              className={mansalva.className}
-              onClick={() => router.push('/')}>
-              &larr; {language.language == 'en' ? 'Go back' : 'Go back, meow'}
-            </span>
-            <UserComponent />
-          </div>
+    <div className={styles.page}>
+      <div style={containerStyle}>
+        <div>
+          <span
+            style={{ cursor: 'pointer', fontSize: '3rem' }}
+            className={mansalva.className}
+            onClick={() => router.push('/')}>
+            &larr; {getText()}
+          </span>
+          <Suspense fallback={<p>Loading user details...</p>}>
+            <UserDetails />
+          </Suspense>
         </div>
       </div>
-    </>
+    </div>
   )
 }
